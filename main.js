50f19db77e955095eaebbac844858dc80eae9ad7
Igor de Oliveira <60223805+igordeoliveira95@users.noreply.github.com>
2021-06-26 13:36:51 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+/*========================open and close menu========================*/
+//will run FOR for each toggle in nav .toggle
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
+
+for (const element of toggle) {
+  element.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+}
+
+//clicking on a link closes the menu
+const links = document.querySelectorAll("nav ul li a");
+
+for (const link of links) {
+  link.addEventListener("click", () => {
+    nav.classList.remove("show");
+  });
+}
+
+/*========================swiper========================*/
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+  loop: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true,
+    },
+  },
+});
+
+/*========================scrollreveal========================*/
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+   `,
+  { interval: 100 }
+);
+
+/*========================scroll functions========================*/
+//add shadow to the header
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
+
+const changeHeaderWhenScroll = () => {
+  if (window.scrollY >= navHeight) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+};
+
+//back to top button
+const toTopButton = document.querySelector(".back-to-top");
+
+const backToTop = () => {
+  if (window.scrollY >= 590) {
+    toTopButton.classList.add("show");
+  } else {
+    toTopButton.classList.remove("show");
+  }
+};
+
+// active navigation menu on desktop version
+const sections = document.querySelectorAll("main section[id]");
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
+//will run above functions when scroll
+window.addEventListener("scroll", () => {
+  changeHeaderWhenScroll();
+  backToTop();
+  activateMenuAtCurrentSection();
+});
